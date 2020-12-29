e5cf20d49abec4799c0a17eae98c0c3965260046
Piotr Boruc <wasilp01@stepstone.com>
2020-12-29 15:25:34 +0100

    added logging to console

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"flag"
+	"log"
 	"net/http"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/hashicorp/consul/api"
 	"github.com/prometheus/client_golang/prometheus"
@@ -14,10 +16,8 @@ import (
 const namespace = "consul_members"
 
 var (
-	listenAddress = flag.String("web.listen-address", ":9142",
-		"Address to listen on for telemetry")
-	metricsPath = flag.String("web.telemetry-path", "/metrics",
-		"Path under which to expose metrics")
+	listenAddress = flag.String("listen-address", ":9142", "Address to listen on for telemetry")
+	metricsPath = flag.String("telemetry-path", "/metrics", "Path under which to expose metrics")
 
 	membersGauge = prometheus.NewDesc(
 		prometheus.BuildFQName(namespace, "", "details"),
@@ -34,18 +34,28 @@ var (
 	}
 )
 
+// Middleware type
+type Middleware func(http.HandlerFunc) http.HandlerFunc
+
+// MiddlewareHandler type
+type MiddlewareHandler func(http.Handler) http.Handler
+
+// Exporter struct
 type Exporter struct {
 	Agent *api.Agent
 }
 
+// NewExporter func
 func NewExporter(agent *api.Agent) *Exporter {
 	return &Exporter{Agent: agent}
 }
 
+// Describe func
 func (e *Exporter) Describe(ch chan<- *prometheus.Desc) {
 	ch <- membersGauge
 }
 
+// Collect func
 func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
 	members, _ := e.Agent.Members(false)
 
@@ -66,6 +76,29 @@ func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
 	}
 }
 
+func logWrapper(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		h.ServeHTTP(w, r) // call original
+		start := time.Now()
+		defer func() {
+			log.Printf("%s %s %s %s %s %s\n", r.Host, r.Method, r.RemoteAddr, r.RequestURI, time.Since(start), r.UserAgent())
+		}()
+	})
+}
+
+func rootHandler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`<html>
+			<head><title>Consul Members Exporter</title></head>
+			<body>
+			<h1>Consul Members Exporter</h1>
+			<p><a href='` + *metricsPath + `'>Metrics</a></p>
+			</body>
+			</html>`))
+	})
+}
+
+
 func main() {
 	flag.Parse()
 
@@ -80,17 +113,10 @@ func main() {
 	exporter := NewExporter(agent)
 	prometheus.MustRegister(exporter)
 
-	http.Handle(*metricsPath, promhttp.Handler())
+	http.Handle("/", logWrapper(rootHandler()))
+	http.Handle(*metricsPath, logWrapper(promhttp.Handler()))
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(`<html>
-			<head><title>Consul Members Exporter</title></head>
-			<body>
-			<h1>Consul Members Exporter</h1>
-			<p><a href='` + *metricsPath + `'>Metrics</a></p>
-			</body>
-			</html>`))
-	})
+	log.Printf("Consul Members Exporter started :: listening on %s\n", *listenAddress)
 
 	http.ListenAndServe(*listenAddress, nil)
 }
